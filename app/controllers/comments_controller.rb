bf1955964701c0548951caeaa443896397e2f5f0
kosukekomuro <mmc.komuro@gmail.com>
2019-04-29 14:17:52 +0900

    add function-edit-user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,6 @@
-class UsersController < ApplicationController
+class CommentsController < ApplicationController
 
-  def edit
+  def index
   end
 
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
 end
