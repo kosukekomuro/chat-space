08d38c13764c823573f038b152669cc0a0de840b
kosukekomuro <mmc.komuro@gmail.com>
2019-05-09 21:36:52 +0900

    Update comments_controller.rb

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,5 @@
 class Api::CommentsController < ApplicationController
   def index
-    @comments = Comment.where("id > ?", "#{params[:id]}")
+    @comments = Comment.where("id > ? AND group_id Like(?)", "#{params[:id]}", "#{params[:group_id]}")
   end
 end
